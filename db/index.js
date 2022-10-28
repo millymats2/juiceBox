945174ee25ba29cde3ac846629a5c0cc7a8bef40
millymats2 <97088594+millymats2@users.noreply.github.com>
2022-10-28 14:23:37 -0700

    completed part 3

--- db/index.js
+++ db/index.js
@@ -3,7 +3,6 @@ const { Client } = require("pg");
 const client = new Client(
   process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev"
 );
-
 async function getAllUsers() {
   try {
     const { rows } = await client.query(
